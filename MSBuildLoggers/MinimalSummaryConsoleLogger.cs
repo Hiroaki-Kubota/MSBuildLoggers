b6dda9d7cd058a529c92a941aea96e497077cca1
Hiroaki-Kubota <foohogehoge@gmail.com>
2016-02-27 20:00:37 +0900

    説明を日本語に変更 リソースファイルで多言語化の練習

--- MSBuildLoggers/MinimalSummaryConsoleLogger.cs
+++ MSBuildLoggers/MinimalSummaryConsoleLogger.cs
@@ -2,18 +2,17 @@
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
-namespace MSBuildLoggers
-{
+namespace MSBuildLoggers {
     /// <summary>
     /// Logger to output minimum and output, a summary to the console.
     /// </summary>
-    public class MinimalSummaryConsoleLogger : Logger
-    {
+    public class MinimalSummaryConsoleLogger : Logger {
+        #region LoggerOption
+
         /// <summary>
         /// Options for <see cref="MinimalSummaryConsoleLogger"/>
         /// </summary>
-        private class Options
-        {
+        private class Options {
             /// <summary>
             /// If you want to show errors, set true. Otherwise false.
             /// </summary>
@@ -30,104 +29,93 @@ namespace MSBuildLoggers
             public string TrimPath = string.Empty;
         }
 
+        #endregion
+
         int warningCount = 0;
         int errorCount = 0;
         Options options = new Options();
-
-        const string HEADER = "Warning/Error\tProject\tFile\t(Line,Column)\tMessage";
-        const string WARNING_MESSAGE = "WARNING\t{0}\t{1}\t({2}, {3})\t{4}";
-        const string ERROR_MESSAGE = "ERROR\t{0}\t{1}\t({2}, {3})\t{4}";
 
         /// <summary>
         /// Subscrive events.
         /// </summary>
         /// <param name="eventSource"></param>
-        public override void Initialize(IEventSource eventSource) 
-        {
+        public override void Initialize(IEventSource eventSource) {
             Console.OutputEncoding = new System.Text.UTF8Encoding();
             ParseParameters();
             eventSource.WarningRaised += eventSource_WarningRaised;
             eventSource.ErrorRaised += eventSource_ErrorRaised;
             eventSource.BuildFinished += eventSource_BuildFinished;
-            Console.WriteLine(HEADER);
+
         }
 
-
-        void ParseParameters()
-        {
-            if (Parameters == null)
-            {
+        void ParseParameters() {
+            if (Parameters == null) {
                 return;
             }
             string[] parameters = Parameters.Split(';');
 
-            foreach (string parameter in parameters)
-            {
-                if (parameter.ToUpper().StartsWith("TRIMPATH"))
-                {
+            foreach (string parameter in parameters) {
+                if (parameter.ToUpper().StartsWith("TRIMPATH")) {
                     int index = parameter.IndexOf('=');
                     this.options.TrimPath = parameter.Substring(index + 1).Trim('"').Trim();
                 }
-                else if (parameter.ToUpper().StartsWith("ENCODING"))
-                {
+                else if (parameter.ToUpper().StartsWith("ENCODING")) {
                     int index = parameter.IndexOf('=');
                     String webname = parameter.Substring(index + 1).Trim('"').Trim();
                     Console.OutputEncoding = System.Text.Encoding.GetEncoding(webname);
                 }
-                else if (parameter.ToUpper() == "HIDEERRORS")
-                {
+                else if (parameter.ToUpper() == "HIDEERRORS") {
                     this.options.ShowErrors = false;
                 }
-                else if (parameter.ToUpper() == "HIDEWARNINGS")
-                {
+                else if (parameter.ToUpper() == "HIDEWARNINGS") {
                     this.options.ShowWarnings = false;
                 }
-                else
-                {
-                    throw new ArgumentException("Unrecognized parameter : ", parameter);
+                else {
+                    throw new ArgumentException(Resources.UnrecognizedParameter, parameter);
                 }
             }
         }
 
-        void eventSource_WarningRaised(object sender, BuildWarningEventArgs e)
-        {
+        void eventSource_WarningRaised(object sender, BuildWarningEventArgs e) {
             warningCount++;
-            if (this.options.ShowWarnings == true)
-            {
-                Console.WriteLine(WARNING_MESSAGE,
+            if (this.options.ShowWarnings == true) {
+                printHeaderOnlyFirstTime();
+                Console.WriteLine(Resources.WARNING_MESSAGE.Replace(@"\t", "\t"),
                                   trimPath(e.ProjectFile),
                                   trimPath(e.File),
                                   e.LineNumber, e.ColumnNumber, e.Message);
             }
         }
 
-        void eventSource_ErrorRaised(object sender, BuildErrorEventArgs e)
-        {
+        void eventSource_ErrorRaised(object sender, BuildErrorEventArgs e) {
             errorCount++;
-            if (this.options.ShowErrors == true)
-            {
-                Console.WriteLine(ERROR_MESSAGE,
+            if (this.options.ShowErrors == true) {
+                printHeaderOnlyFirstTime();
+                Console.WriteLine(Resources.ERROR_MESSAGE.Replace(@"\t", "\t"),
                                   trimPath(e.ProjectFile),
                                   trimPath(e.File),
                                   e.LineNumber, e.ColumnNumber, e.Message);
             }
         }
 
-        void eventSource_BuildFinished(object sender, BuildFinishedEventArgs e)
-        {
+        void eventSource_BuildFinished(object sender, BuildFinishedEventArgs e) {
             Console.WriteLine("");
-            Console.WriteLine("Processing completed: " + errorCount + " error(s) | " + warningCount + " warning(s)");
+            Console.WriteLine(Resources.Completed, errorCount, warningCount);
             Console.WriteLine("");
         }
 
-        string trimPath(string path)
-        {
-            if (string.IsNullOrWhiteSpace(this.options.TrimPath))
-            {
+        void printHeaderOnlyFirstTime() {
+            if ((this.options.ShowErrors && this.errorCount == 1) ||
+                (this.options.ShowWarnings && this.warningCount == 1)) {
+                Console.WriteLine(Resources.HEADER.Replace(@"\t", "\t"));
+            }
+        }
+
+        string trimPath(string path) {
+            if (string.IsNullOrWhiteSpace(this.options.TrimPath)) {
                 return path;
             }
-            else
-            {
+            else {
                 return path.Replace(this.options.TrimPath, string.Empty);
             }
         }
--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # MSBuildLoggers
 
-Custom logger for MSBuild over Jenkins.
+プログラム練習を兼ねたMSBuild用のロガーです。
+JenkinsでビルドプロジェクトへこのDLLを配備するには、
+Custom Tools Plugin (https://wiki.jenkins-ci.org/display/JENKINS/Custom+Tools+Plugin) を使用すると簡単です。
 
 ## MinimalSummaryConsoleLogger
 
-This logger only outputs errors and warnings.
-Each item separeted by tab.
+このロガーはエラー・警告の出力とサマリーの出力を行います。
+各項目をタブ区切りで出力しているのでExcelへコピペしてフィルタできます。
+また、既定のエンコードをUTF-8としているため、JenkinsのMSBuild Puginを使用しても日本語が文字化けしません。
 
 ### Example for jenkins
 
-MSBuild task arguments:
+MSBuild タスクの引数に次のように記述します:
 /noconsolelogger /logger:MinimalSummaryConsoleLogger,%MSBuildLoggers_HOME%\MSBuildLoggers.dll;TrimPath="%WORKSPACE%\\"
